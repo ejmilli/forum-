de5409b65bcdf89525de413a6a0eea479ae307d1
Eliza John <112093038+ejmilli@users.noreply.github.com>
2025-01-15 22:21:57 +0200

    made changes for html files and also created some backend to connect it to server such as main.go and pages.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"forum/utils"
+	"net/http"
+)
+
+func main() {
+
+	utils.InitializeHtml()
+	http.HandleFunc("/login", utils.LoginPage)
+	http.HandleFunc("/signup", utils.SignUpPage)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	fmt.Println("Starting Forum on http://localhost:8080/...")
+	http.ListenAndServe(":8080", nil)   
+}
---
+++ utils/pages.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+
+func InitializeHtml() {
+ 
+   var err error
+	tpl, err = template.ParseGlob("template/*.html")
+	if err != nil {
+		fmt.Printf("Error parsing templates: %v", err)
+	}
+
+}
+
+func LoginPage(w http.ResponseWriter, r *http.Request) {
+
+err := tpl.ExecuteTemplate(w, "login.html", nil)
+if err != nil {
+	log.Fatal("error executing login page ")
+}
+}
+
+
+func SignUpPage(w http.ResponseWriter, r *http.Request) {
+
+	err := tpl.ExecuteTemplate(w, "signup.html", nil)
+	if err != nil {
+		log.Fatal("error executing login page ")
+	}
+	}
